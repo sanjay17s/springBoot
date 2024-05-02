7ab5bdbb7f2e4ab8b6a8dd7de564f3ff0b923ea3
sanjay17s <sanjay.17suresh2000@gmail.com>
2024-05-02 18:36:33 +0530

    springboot revision

---
+++ src/main/java/com/example/springboot/revison/SpringbootRevisonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.revison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRevisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRevisonApplication.class, args);
+	}
+
+}
